3c1e4e18c979093b9a390b1d613eb16f023c0eb6
Eric Craft <eacraft13@gmail.com>
2015-11-17 15:41:43 -0800

    Trying to fix http requests

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-var _ = require('lodash');
-var async = require('async');
-var request = require('request');
-var r = require('rethinkdb');
+var _           = require('lodash');
+var async       = require('async');
+var http        = require('http');
+var ocpHeaders  = { 'Ocp-Apim-Subscription-Key': 'f05dcf2fe1934b4197174577dfe3068b' };
+var rOptions    = { host: '45.56.91.5', port: '28015' };
+var r           = require('rethinkdb');
+var request     = require('request');
 
-var headers = { 'Ocp-Apim-Subscription-Key': 'f05dcf2fe1934b4197174577dfe3068b' };
+
+
 var stats = [
     require('./lib/final_box_scores'),
     // require('./lib/game_stats'),
@@ -12,13 +16,8 @@ var stats = [
 ];
 
 
-var options = {
-    host: '45.56.91.5',
-    port: '28015'
-};
 
-
-r.connect(options, function(err, conn) {
+r.connect(rOptions, function(err, conn) {
     if (err) throw err;
 
     _.each(stats, function(stat) {
@@ -26,16 +25,24 @@ r.connect(options, function(err, conn) {
         var urls = _.flattenDeep(stat.urls);
         _.each(urls, function(url) {
 
-            console.log(url);
-            request({ url: url, headers: headers }, function(error, response) {
-                console.log(JSON.parse(response));
-                if (error) console.log(error);
-                r.db(stat.db).table(stat.table)
-                .insert(JSON.parse(response.body))
-                .run(conn, function(err, result) {
-                    if (err) throw err;
-                    console.log(JSON.stringify(result, null, 2));
-                });
+            request
+            //.get({ url: url, headers: ocpHeaders })
+            //.get({ url: 'https://www.google.com', headers: headers })
+            .get('http://www.google.com')
+            .on('error', function(err) {
+                console.log('error: ' + err);
+            })
+            .on('response', function(response) {
+                console.log('response: ' + response);
+
+            //    //r
+            //    //.db(stat.db)
+            //    //.table(stat.table)
+            //    //.insert(JSON.parse(response.body))
+            //    //.run(conn, function(err, result) {
+            //    //    if (err) throw err;
+            //    //    console.log(JSON.stringify(result, null, 2));
+            //    //});
             });
 
         });
