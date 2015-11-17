e48b8a3465bb0d75e660b0b8032b08db4165b129
Eric Craft <eacraft13@gmail.com>
2015-11-17 06:34:55 -0800

    Setup to start scraping data

--- index.js
+++ index.js
@@ -1,46 +1,47 @@
 var _ = require('lodash');
+var async = require('async');
 var request = require('request');
 var r = require('rethinkdb');
 
+var headers = { 'Ocp-Apim-Subscription-Key': 'f05dcf2fe1934b4197174577dfe3068b' };
+var stats = [
+    require('./lib/final_box_scores'),
+    // require('./lib/game_stats'),
+    require('./lib/players'),
+    require('./lib/season_league_leaders')
+];
 
 
-var fantasyUrls = {
-    gameStats: 'https://api.fantasydata.net/nfl/v2/JSON/GameStats/2015',
-    gameStatsByPlayer: 'https://api.fantasydata.net/nfl/v2/JSON/PlayerGameStatsByPlayerID/2015/6/4314'
-};
-
-var rOptions = {
+var options = {
     host: '45.56.91.5',
     port: '28015'
 };
 
 
-r.connect(rOptions, function(err, conn) {
-    if (err) console.log(err);
+r.connect(options, function(err, conn) {
     if (err) throw err;
 
-    var options = {
-        url: fantasyUrls.gameStats,
-        headers: { 'Ocp-Apim-Subscription-Key': 'f05dcf2fe1934b4197174577dfe3068b' }
-    };
+    _.each(stats, function(stat) {
 
+        var urls = _.flattenDeep(stat.urls);
+        _.each(urls, function(url) {
 
-
-    request(options, function(error, response, body) {
-        if (error) throw error;
-
-        var results = JSON.parse(response.body);
-
-        _.forEach(results, function(val, key) {
-            r.db('nfl').table('game_stats').insert(val)
-            .run(conn, function(err, result) {
-                if (err) throw err;
-                console.log(JSON.stringify(result, null, 2));
+            console.log(url);
+            request({ url: url, headers: headers }, function(error, response) {
+                console.log(JSON.parse(response));
+                if (error) console.log(error);
+                r.db(stat.db).table(stat.table)
+                .insert(JSON.parse(response.body))
+                .run(conn, function(err, result) {
+                    if (err) throw err;
+                    console.log(JSON.stringify(result, null, 2));
+                });
             });
+
         });
 
-        console.log('Complete!');
-        process.exit();
     });
 
+    console.log('Complete!');
+    process.exit();
 });
