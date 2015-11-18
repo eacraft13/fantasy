48bf1db75b230ad15b1f143f88c15bac7c6c2168
Eric Craft <eacraft13@gmail.com>
2015-11-17 16:44:58 -0800

    Complete ocp scraping tool

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 var _           = require('lodash');
 var async       = require('async');
-var http        = require('http');
 var ocpHeaders  = { 'Ocp-Apim-Subscription-Key': 'f05dcf2fe1934b4197174577dfe3068b' };
 var rOptions    = { host: '45.56.91.5', port: '28015' };
 var r           = require('rethinkdb');
@@ -9,10 +8,10 @@ var request     = require('request');
 
 
 var stats = [
-    require('./lib/final_box_scores'),
-    // require('./lib/game_stats'),
-    require('./lib/players'),
-    require('./lib/season_league_leaders')
+    //require('./lib/final_box_scores'),
+    //require('./lib/game_stats'),
+    //require('./lib/players'),
+    //require('./lib/season_league_leaders')
 ];
 
 
@@ -20,35 +19,35 @@ var stats = [
 r.connect(rOptions, function(err, conn) {
     if (err) throw err;
 
+    var requests = [];
+
     _.each(stats, function(stat) {
 
         var urls = _.flattenDeep(stat.urls);
         _.each(urls, function(url) {
+            requests.push(function(callback) {
+                request({ url: url, headers: ocpHeaders }, function(err, response) {
+                    if (err) throw err;
 
-            request
-            //.get({ url: url, headers: ocpHeaders })
-            //.get({ url: 'https://www.google.com', headers: headers })
-            .get('http://www.google.com')
-            .on('error', function(err) {
-                console.log('error: ' + err);
-            })
-            .on('response', function(response) {
-                console.log('response: ' + response);
-
-            //    //r
-            //    //.db(stat.db)
-            //    //.table(stat.table)
-            //    //.insert(JSON.parse(response.body))
-            //    //.run(conn, function(err, result) {
-            //    //    if (err) throw err;
-            //    //    console.log(JSON.stringify(result, null, 2));
-            //    //});
+                    r
+                    .db(stat.db)
+                    .table(stat.table)
+                    .insert(JSON.parse(response.body))
+                    .run(conn, function(err, result) {
+                        if (err) throw err;
+                        console.log('Inserted', result.inserted, 'rows to', stat.table);
+                        callback(null, JSON.stringify(result, null, 2));
+                    });
+                });
             });
 
         });
 
     });
 
-    console.log('Complete!');
-    process.exit();
+    async.parallel(requests, function(err, results) {
+        if (err) throw err;
+        console.log('Success!');
+        process.exit();
+    });
 });
